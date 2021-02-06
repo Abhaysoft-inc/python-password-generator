7accbe4d3a7bb67055aedfae172920fb9c1c9f5e
Abhay Vishwakarma <av385735@gmail.com>
2021-02-07 04:48:32 +0530

    Done

---
+++ Python-Password-Generator/main.py
@@ -0,0 +1,11 @@
+import random
+import pyperclip
+from tkinter import *
+from tkiner.ttk import *
+
+# Function of Calculation Password
+
+def low():
+    entry.delete(0, END)
+
+    # Get the length of the password
